0cb2ec2207ce41e3223094517a9886d14ba1cbc9
kilodayt <kilodayt@mail.ru>
2025-12-01 17:40:42 +0300

    model Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,2 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Task(models.Model):
+    TODO = 'todo'
+    IN_PROGRESS = 'in_progress'
+    DONE = 'done'
+
+    STATUS_CHOICES = [
+        (TODO, 'TO DO'),
+        (IN_PROGRESS, 'In Progress'),
+        (DONE, 'Done')
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default=TODO)
+    due_date = models.DateField()
+    owner = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} {str(self.status)}'
 
